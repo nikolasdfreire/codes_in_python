90791a66ffe07ab6a15d6f5f8f6a2cf56ee08e5a
nikolasdfreire <nikolasdfreire@gmail.com>
2022-09-05 23:30:59 -0300

    Add a new file about inputs

---
+++ basic/3_name.py
@@ -0,0 +1,8 @@
+name = str(input('Your name: '))
+last_name = str(input('Last name: '))
+print(f'Your name is {name} {last_name} ')
+
+
+nome=input('Digite seu primeiro nome: ').strip().title()
+sobrenome=input('Digite seu sobrenome: ').strip().title()
+print(f'{nome} {sobrenome}')
